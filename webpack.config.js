ea5e9013b8cd284ae3fc9ae4e6c68eab472ae348
Shaheen Siddique <shaheen.siddique@wipro.com>
2018-07-20 14:06:34 +0000

    Added support for Vue compiler

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
